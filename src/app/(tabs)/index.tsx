8c241e9a49b8d32b5ae68cddc72394ad22f72559
Antonio Vuono <antoniosvuono@icloud.com>
2024-01-05 23:11:36 -0300

    feature: music player ui finalized

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { FlatList } from "react-native";
 
 import { tracks } from "../../../assets/data/tracks";
+import Player from "../../components/Player";
 import TrackListItem from "../../components/TrackListItem";
 
 export default function HomeScreen() {
